ba4e3102c60de5b584559d2f043982514dd7a0d5
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-03 13:21:54 +0800

    Update Hello.py

--- Python_Introduction/Hello.py
+++ Python_Introduction/Hello.py
@@ -1,2 +1,9 @@
-#Its always great to start learning a new programming language, by always putting in the old fashion hello world. 
+#Its always great to start learning a new programming language, by always putting in the old fashion hello world.  
+#Print is a function, arugments can be passed into the function.
 print("Hello World")
+
+#Using print function to perform addition.
+print(1+2)  
+
+#You the print functions to print the my name 
+print("Hello my name is Josh")
