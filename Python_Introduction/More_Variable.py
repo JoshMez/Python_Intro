f9c594ba36c02afa703034c55a6d435e5f9e702d
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-04 15:51:28 +0800

    Update and rename file4.py to More_Variable.py

---
+++ Python_Introduction/More_Variable.py
@@ -0,0 +1,8 @@
+#This script will contain more information on variables. 
+
+#Better way to write a long number. 
+Max = 100_000_120 
+
+#Writing a constant. 
+#A variable can be change at any moment but a great way to store a CONSTANT. 
+CONSTANT = "HELLO WORLD"  
