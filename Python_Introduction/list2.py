4d619568372f50d1124a624d725cd886bf5c5cd0
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-05 10:56:06 +0800

    Update list2.py

--- Python_Introduction/list2.py
+++ Python_Introduction/list2.py
@@ -2,3 +2,24 @@
 #You can create an empty lists and append items to it. 
 #Create an empty lists. 
 fav_cars = []
+
+#appending items to an empty list.
+fav_cars.append("Honda")
+fav_cars.append("Lambo")
+fav_cars.append("Honda Civic")
+
+#Printing out the new list.
+print(fav_cars)
+print("########################")
+print()
+#Inserting elements into a lists at any postiion, just be mindful of the syntax and structure.
+#When inserting the structure, always put the INDEX NUMBER
+fav_cars.insert(0, "Tesla".upper())
+print(fav_cars)
+
+#Removing items from a lists. #When removing an item from a list, used del <list_name>[INDEX_POSITION] <-
+del fav_cars[0]
+print(fav_cars)
+#Key word to remeber here is: "del"
+del fav_cars[-1]
+print(fav_cars)
