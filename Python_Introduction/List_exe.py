793eb9950cc9740c8ad1dcf34166363ad83af91e
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-08 17:52:50 +0800

    Update and rename file7.py to List_exe.py

---
+++ Python_Introduction/List_exe.py
@@ -0,0 +1,35 @@
+#Think of at least 5 places in the world you would like to visit.
+
+Places = ["Spain",
+          "Australia",
+          "Isreal",
+          "USA",
+          "South Korea",
+          "Dubai"
+          ]
+#Print your list in the original order.
+print(Places)
+print()
+#Print the list in Alphabetical order.
+print(sorted(Places))
+print()
+#Printing the origianl lists
+print(Places)
+print()
+#Printing the list in reverse Alphabetical Order.
+print(sorted(Places))
+print()
+#Printing the original list.
+print(Places)
+print()
+#Printing the list with the reverse
+Places.reverse()
+print(Places)
+print()
+#Shwoing that the list is now in a reverse order.
+print(Places)
+print()
+#Reversing the list once again so that it is in its original form.
+Places.reverse()
+print(Places)
+print()  
