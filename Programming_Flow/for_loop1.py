0a8173b09af02e1b33b1dabc9558e2bb3b38c4f0
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-16 00:56:51 +0800

    Update and rename file10.py to for_loop1.py

---
+++ Programming_Flow/for_loop1.py
@@ -0,0 +1,23 @@
+#Basic on using the for loop.
+#Remember the colon at the end means that, this is the start of the for loop.
+#Forgetting to put the semi- colon will result in a syntax error.
+
+#Using a for loop to statment my favorite pizza.
+#Making a list of my favorite pizza's
+pizzas = ["Something Meaty", "Four seasons", "Vegetarian"]
+
+for pizza in pizzas:
+    print(f"My favorite pizza is {pizza}")
+
+print("There you have it, i have listed down my favorite pizza's")
+print()
+print()
+#
+#
+#Making a list of my dogs at home 
+#Using a for loop to go through those strings. 
+
+dogs = ["Kush", "Russel", "Fluffy","Pushkin","Bobby"]
+for dog in dogs:
+    print(f"{dog} has a special place in our family") 
+print("Dogs are the freaking best")
