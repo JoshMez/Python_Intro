a15fcba5453ff80f1f1275d279fff1eee6f67ffe
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-16 01:17:54 +0800

    Update and rename file3.py to Number_Lists.py

---
+++ Programming_Flow/Number_Lists.py
@@ -0,0 +1,6 @@
+#Creating lists that contain number in Python is very important. 
+
+#Making a list with even numbers in it. 
+even_numbers = list(range(0,11,2)) #The list function has been used to say that a lists is being created. #Passing the range arguments in that , stating arugments in that. 
+print(even_numbers)  
+
