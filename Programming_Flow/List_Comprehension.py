553677ca30b6f673e7163d435608f2a39483e8cc
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-18 00:48:42 +0800

    Update and rename file4.py to List_Comprehension.py

---
+++ Programming_Flow/List_Comprehension.py
@@ -0,0 +1,9 @@
+#Basic demonstration of list comprehension. 
+#Variable was declared. 
+#List was create with the square brackets. 
+#value ** 2 was created. 
+#The a for loop was created ; NOTICE : THere was not the semi:colon as the end. 
+
+squares = [ value ** 2 for value in range(1,11) ] 
+print(squares)
+
