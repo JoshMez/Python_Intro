7a62d9ac3cb0aaa2773755f774a245924f0c69d1
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-21 18:11:28 +0800

    Update and rename file7.py to Conditional_Statement.py

---
+++ Programming_Flow/Conditional_Statement.py
@@ -0,0 +1,45 @@
+#Conditional Statements Tests.
+#
+#Condtional Statments tests.
+print("The aim of this programme is to test mulitple conditions of statements. \n\n\n")
+#
+print("The 1st Test.")
+print("The car the use must try and guess what the car is")
+#
+car = "Honda"
+print("Is car == Honda ? I can predict true")
+print("Is the car a Honda:", car == "Honda")
+print()
+print("Is car == Toyota", car == "Toyota")
+print("Is car a Toyota:", car == "Toyota")
+#
+print()
+#
+print("This tests if to check if the fav pizza is Meaty or Vegetarian.")
+pizza = "Meaty"
+print("Is Pizza == Meaty")
+print(pizza == "Meaty\n")
+print("Is Pizza == Vegetarian")
+print(pizza == "Vegetarian")
+print("\n \n", "*" * 50, " Finsished ", "*" * 50)
+print()
+print("What is you favorite restaurant. Kulture or Mad Max")
+fav_restaur = "Kulture"
+print("Is you fav restuarance Kulture")
+print(fav_restaur == "Kulture")
+print()
+print("Is your favorite resturant Mad Mex")
+print(fav_restaur == "Mad Mex")
+print("\n \n", "*" * 50, " Finsished ", "*" * 50, "\n\n")
+#
+print("Favorite Ice cream?, Chocolate or Vanilla? \n")
+fav_ice_cream = "Vanilla"
+#
+print("Is your favorite ice cream vanilla")
+print(fav_ice_cream == "Vanilla\n")
+#
+print("Is your favorite ice cream chocolate ?")
+print(fav_ice_cream == "Chocolate\n")
+#
+print("\n \n", "*" * 50, " Finished ", "*" * 50)
+#
