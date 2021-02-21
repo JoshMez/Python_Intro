11f89cdf2cf2519f547fa8b4eb97b477d1f0e19a
Joshua <65163785+JoshMez@users.noreply.github.com>
2021-02-21 23:31:12 +0800

    Update Beginner_List.py

--- List/Beginner_List.py
+++ List/Beginner_List.py
@@ -1,16 +1,16 @@
-current_choice = "-" 
-computer_list = [] 
-#  
-while current =! 0: 
-  if current_choice in "12345": 
-    print(f"Adding {current_choice})
-    
-  else:  
-    print("Please add an option from the list below") 
-    print("1: Computer") 
-    print("2: monitor") 
-    print("3: Keyboard") 
-    print("4: Mouse") 
-    print("5: Mouse Mat") 
-    print("0: Finish") 
-    
+current_choice = "-"
+computer_list = []
+#
+while current_choice != 0:
+    if current_choice in "12345":
+        print(f"Adding {current_choice}")
+
+    else:
+        print("Please add an option from the list below")
+    print("1: Computer")
+    print("2: monitor")
+    print("3: Keyboard")
+    print("4: Mouse")
+    print("5: Mouse Mat")
+
+    current_choice = input("Please add the option you would like ?")
